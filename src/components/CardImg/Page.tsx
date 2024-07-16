b381d0382ba9a3138dcec49e0eaa159fa78edac4
Michaeleduardoo <michaeledu20@outlook.com>
2024-07-16 15:52:14 -0300

    Ajuste no codigo

--- src/components/CardImg/Page.tsx
+++ src/components/CardImg/Page.tsx
@@ -1,44 +1,44 @@
-import React, { useState } from 'react';
-import Image1 from '../image/image1.png';
-import Image2 from '../image/image2.png';
-import Image3 from '../image/image3.png';
-import Image4 from '../image/image4.png';
+import Image1 from "../image/image1.png";
+import Image2 from "../image/image2.png";
+import Image3 from "../image/image3.png";
+import Image4 from "../image/image4.png";
+import '../../index.css'; // Importe seu arquivo de estilos CSS aqui
 
 interface CustomCSSProperties extends React.CSSProperties {
   '--i': number;
 }
 
-function App() {
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+const App: React.FC = () => {
 
-  const toggleActive = (index: number) => {
-    setActiveIndex(activeIndex === index ? null : index);
+  const toggleImageBox = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const target = event.currentTarget;
+    target.classList.toggle('active');
   };
 
   return (
     <div className="container">
-      <div className={`box ${activeIndex === 1 ? 'active' : ''}`} style={{ '--i': 3 } as CustomCSSProperties}>
-        <div className="imgBx" onClick={() => toggleActive(1)}>
+      <div className="box" style={{ '--i': 3 } as CustomCSSProperties}>
+        <div className="imgBx" onClick={toggleImageBox}>
           <img src={Image1} alt="Imagem 1" />
         </div>
       </div>
-      <div className={`box ${activeIndex === 2 ? 'active' : ''}`} style={{ '--i': 4 } as CustomCSSProperties}>
-        <div className="imgBx" onClick={() => toggleActive(2)}>
+      <div className="box" style={{ '--i': 4 } as CustomCSSProperties}>
+        <div className="imgBx" onClick={toggleImageBox}>
           <img src={Image2} alt="Imagem 2" />
         </div>
       </div>
-      <div className={`box ${activeIndex === 3 ? 'active' : ''}`} style={{ '--i': 5 } as CustomCSSProperties}>
-        <div className="imgBx" onClick={() => toggleActive(3)}>
+      <div className="box" style={{ '--i': 5 } as CustomCSSProperties}>
+        <div className="imgBx" onClick={toggleImageBox}>
           <img src={Image3} alt="Imagem 3" />
         </div>
       </div>
-      <div className={`box ${activeIndex === 4 ? 'active' : ''}`} style={{ '--i': 6 } as CustomCSSProperties}>
-        <div className="imgBx" onClick={() => toggleActive(4)}>
+      <div className="box" style={{ '--i': 6 } as CustomCSSProperties}>
+        <div className="imgBx" onClick={toggleImageBox}>
           <img src={Image4} alt="Imagem 4" />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
