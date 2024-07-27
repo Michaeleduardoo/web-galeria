41d5429842859e2825650866e81dafd4d7fbf566
Michael Eduardo <106412874+Michaeleduardoo@users.noreply.github.com>
2024-07-27 09:15:10 -0300

    Update Page.tsx

--- src/components/Footer/Page.tsx
+++ src/components/Footer/Page.tsx
@@ -1,17 +1,51 @@
-import React from 'react';
+import React from "react";
 
 interface CustomCSSProperties extends React.CSSProperties {
-  '--i': number;
+  "--i": number;
 }
 
 function App() {
   return (
     <div className="footer">
       <ul className="sci">
-        <li><a href="#" style={{ '--i': 9 } as CustomCSSProperties}>Facebook</a></li>
-        <li><a href="#" style={{ '--i': 8 } as CustomCSSProperties}>Twitter</a></li>
-        <li><a href="#" style={{ '--i': 7 } as CustomCSSProperties}>Instagram</a></li>
+        <li>
+          <a
+            href="#"
+            data-tooltip="Casal: Ana e Michael"
+            style={{ "--i": 9 } as CustomCSSProperties}
+          >
+            Casal
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            data-tooltip="Cachorro 1: Bidu"
+            style={{ "--i": 8 } as CustomCSSProperties}
+          >
+            Cachorro 1
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            data-tooltip="Cachorro 2: Tody"
+            style={{ "--i": 7 } as CustomCSSProperties}
+          >
+            Cachorro 2
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            data-tooltip="Cachorro 3: Babi"
+            style={{ "--i": 6 } as CustomCSSProperties}
+          >
+            Cachorro 3
+          </a>
+        </li>
       </ul>
+
       <p className="copyrightText">© WebCode. 2024</p>
     </div>
   );
